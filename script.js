148ad8d16b8f85c06c9c34ff3a2684581b04a9f6
Leonardo Nunes <leonardoportelanunes@gmail.com>
2022-06-09 08:52:44 -0400

    Desafio 01-Mod2

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome='Leonardo'
+let num=8
+let anoAtual=2022
+let calculo=anoAtual-num
+console.log(anoAtual+num)
+console.log('Estou mexendo no console.log')
+
+let quartaFeira=true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
